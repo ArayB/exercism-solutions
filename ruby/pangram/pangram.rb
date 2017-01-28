1d01b98f8c7b0ade009a164f5be44ad9d696e613
Ant Barnes <ant@yoomee.com>
2017-01-28 21:15:40 +0000

    Remove length check
    Length check added no real benefit.
    .all? method will short circuit evaluation as soon as a letter does not
    match.

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,8 +1,5 @@
 class Pangram
-  ALPHABET_LENGTH = 26
-
   def self.pangram?(input)
-    return false if input.length < ALPHABET_LENGTH
     input = input.downcase
     ('a'..'z').all? { |letter| input.include?(letter) }
   end
